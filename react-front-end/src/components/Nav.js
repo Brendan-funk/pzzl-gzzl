a23907a84665c5db9dae25b47b55e2537aaf3c55
Ethan Loewen <ethanloewen@gmail.com>
2022-04-18 22:44:34 +0000

    Finish simple nav

--- react-front-end/src/components/Nav.js
+++ react-front-end/src/components/Nav.js
@@ -1,6 +1,10 @@
 import React from "react";
 import './Nav.scss'
 
+// get our fontawesome imports
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTrophy } from "@fortawesome/free-solid-svg-icons";
+
 export default function Nav(props) {
 
   return (
@@ -8,6 +12,9 @@ export default function Nav(props) {
       <ul>
         <li><a href="#/">Logo</a></li>
         <li><a href="#/">tagline</a></li>
+        <span id='trophy-icon'>
+          <FontAwesomeIcon icon={faTrophy}/>
+        </span>
       </ul>
     </nav>
   );
