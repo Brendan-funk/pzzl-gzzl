88087014acbc88b8d58529f4da3292c3a22da9f6
Ethan Loewen <ethanloewen@gmail.com>
2022-04-18 22:52:04 +0000

    Make menu componenet

---
+++ react-front-end/src/components/Menu.js
@@ -0,0 +1,18 @@
+import React from "react";
+import './Menu.scss'
+
+// get our fontawesome imports
+// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+// import { faTrophy } from "@fortawesome/free-solid-svg-icons";
+
+export default function Menu(props) {
+
+  return (
+    <div id='menu'>
+      <span>a</span>
+      <span>b</span>
+      <span>c</span>
+      <span>d</span>
+    </div>
+  );
+}
