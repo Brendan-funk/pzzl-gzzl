23b8d73a542eb6860617bca04871f66258d8bcb6
denda <brendanzfunk@gmail.com>
2022-04-22 14:49:52 -0700

    adds db connection to update user rating on completing a puzzle

--- express-back-end/server.js
+++ express-back-end/server.js
@@ -3,13 +3,19 @@ const App = Express();
 const BodyParser = require('body-parser');
 const PORT = 8001;
 const testRoutes = require('./routes/sudoku');
+const cors = require('cors');
 // Express Configuration
 App.use(BodyParser.urlencoded({ extended: false }));
 App.use(BodyParser.json());
 App.use(Express.static('public'));
-
+App.use(
+  cors({
+    origin: "*",
+  })
+)
 //Database Setup
 const { Pool } = require("pg");
+const bodyParser = require('body-parser');
 const db = new Pool( {
   host: 'localhost',
   port: 5432,
@@ -27,6 +33,24 @@ App.get('/api/data', (req, res) => res.json({
 }));
 App.use("/sudoku", testRoutes('ginga'));
 
+App.post('/rating', (req, res) => {
+  let tempRating = 0;
+  let bodyObj = req.body;
+  const ratingChange = bodyObj.ratingChange;
+  const userId = bodyObj.id;
+  db.query(`SELECT rating FROM users WHERE id = $1`, [userId])
+  .then(temp_rating => {
+    console.log(temp_rating.rows[0].rating);
+    tempRating = temp_rating.rows[0].rating;
+    newRating = tempRating + ratingChange;
+    db.query(`UPDATE users
+      SET rating = $1
+      WHERE id = $2`, [newRating, userId])
+  });
+});
+App.get('/rating', (req,res) => {
+  console.log('works');
+})
 App.listen(PORT, () => {
   // eslint-disable-next-line no-console
   console.log(`Express seems to be listening on port ${PORT} so that's pretty good 👍`);
