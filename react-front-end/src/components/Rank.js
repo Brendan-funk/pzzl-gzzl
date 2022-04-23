14186901a2fa4d8b6f7819cb4ca6ec251e17c81c
ethanloewen <ethanloewen@gmail.com>
2022-04-22 19:57:32 -0700

    Style Ranke Rating card

---
+++ react-front-end/src/components/Rank.js
@@ -0,0 +1,20 @@
+import React from "react";
+import './Rank.scss'
+
+export default function Rank(props) {
+
+  return (
+    <div id='blur-buddy'>
+      <div id="rank">
+        <div id='rank-title'>
+          <h1>Ranked Rating</h1>
+          <p>203</p>
+        </div>
+        <div class='wrapper'>
+          <h2>+24</h2>
+          <button class="button-54">continue</button>
+        </div>
+      </div>
+    </div>
+  );
+}
