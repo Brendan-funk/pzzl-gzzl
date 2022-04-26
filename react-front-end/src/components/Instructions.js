1e72a0058ea45c4b2d5cef81f8baf8bf33069677
ethanloewen <ethanloewen@gmail.com>
2022-04-26 10:02:04 -0700

    Add a loader for the 3d menu

--- react-front-end/src/components/Instructions.js
+++ react-front-end/src/components/Instructions.js
@@ -1,20 +1,15 @@
 import React from "react";
 import './Instructions.scss';
-import classNames from "classnames";
 
 // get our fontawesome imports
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faXmark } from "@fortawesome/free-solid-svg-icons";
 
 export default function Popup(props) {
-  // set class name for the popup
-  const instructionsClass = classNames('instructions', {
-    'instructions--hide': props.hide
- });
 
   return (
     <div className="instructions-wrap">
-      <div className={instructionsClass} onClick={props.hideInstructions}>
+      <div className="instructions" onClick={props.hideInstructions}>
         <FontAwesomeIcon icon={faXmark} id='x-left'/>
           <h1>
             Select a puzzle with the arrow keys and hit enter to play!
--- react-front-end/src/components/Menu3D.js
+++ react-front-end/src/components/Menu3D.js
@@ -7,6 +7,7 @@ import Instructions from "./Instructions";
 import * as THREE from 'three';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+import { ColladaLoader } from 'three/examples/jsm/loaders/ColladaLoader';
 
 import { TTFLoader } from 'three/examples/jsm/loaders/TTFLoader';
 import { FontLoader } from 'three/examples/jsm/loaders/FontLoader';
@@ -37,26 +38,20 @@ export default function Menu(props) {
     );
     camera.position.set(0, 25, 35);
 
-    // lighting
-    hemiLight = new THREE.HemisphereLight(0xffeeb1, 0x080820, 1.5);
-    scene.add(hemiLight);
+    const loadingManager = new THREE.LoadingManager( () => {
+	
+      const loadingScreen = document.getElementById( 'loading-screen' );
+      loadingScreen.classList.add( 'fade-out' );
+      
+      // // optional: remove loader from DOM via event listener
+      loadingScreen.addEventListener( 'transitionend', onTransitionEnd );
+      
+    } );
 
+    // collada
 
-    renderer = new THREE.WebGLRenderer({ antialias: true });
-    renderer.setPixelRatio( window.devicePixelRatio );
-    renderer.setSize(window.innerWidth, window.innerHeight);
-    document.body.appendChild(renderer.domElement);
-
-
-    const light = new THREE.DirectionalLight( 0xFFFFFF, 0.5 );
-    light.position.set(0, 25, 30);
-    scene.add( light );
-
-    //----------------------------
-    // --- helpers for testing ---
-    //----------------------------
-    // scene.add(new THREE.AxesHelper(500));
-    // const controls = new OrbitControls( camera, renderer.domElement );
+	  const colladaLoader = new ColladaLoader( loadingManager );
+	  colladaLoader.load( 'https://threejs.org/examples/models/collada/stormtrooper/stormtrooper.dae', ( collada ) => {
 
     // ----------------------------
     // --- custom model loading ---
@@ -227,8 +222,6 @@ export default function Menu(props) {
       scene.add(textMesh);
     });
 
-    // generate backgroud puzzle pieces
-
     for (let i = 0; i < 500; i++) {
       // eslint-disable-next-line no-loop-func
       loader.load('models/puzzle_piece.gltf', (gltf) => {
@@ -239,6 +232,211 @@ export default function Menu(props) {
           scene.add(object);
       });
     }
+
+	} );
+
+    // lighting
+    hemiLight = new THREE.HemisphereLight(0xffeeb1, 0x080820, 1.5);
+    scene.add(hemiLight);
+
+
+    renderer = new THREE.WebGLRenderer({ antialias: true });
+    renderer.setPixelRatio( window.devicePixelRatio );
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    document.body.appendChild(renderer.domElement);
+
+
+    const light = new THREE.DirectionalLight( 0xFFFFFF, 0.5 );
+    light.position.set(0, 25, 30);
+    scene.add( light );
+
+    //----------------------------
+    // --- helpers for testing ---
+    //----------------------------
+    // scene.add(new THREE.AxesHelper(500));
+    // const controls = new OrbitControls( camera, renderer.domElement );
+
+    // // ----------------------------
+    // // --- custom model loading ---
+    // // ----------------------------
+    
+    // // new group for spinner models
+    // spinnerGroup = new THREE.Group();
+
+    // // head model
+    // var loader = new GLTFLoader();
+    // loader.load('models/puzz_head_v2.gltf', function(gltf) {
+    //   var object = gltf.scene;
+    //   object.position.set(0.5, 12, -5.5);
+    //   object.scale.set(0.5, 0.5, 0.5);
+    //   object.rotation.x -= 0.07; // adjust rotation for imported model (blender issue)
+    //   scene.add(object);
+    // });
+
+    // // base model
+    // loader.load('models/puzz_bottom_v2.gltf', function(gltf) {
+    //   var object = gltf.scene;
+    //   object.position.set(-0.2, 16.5, 0);
+    //   object.scale.set(0.15, 0.10, 0.15);
+    //   // object.rotation.x -= 0.07; // adjust rotation for imported model (blender issue)
+    //   scene.add(object);
+    // });
+
+    // // middle spinner
+    // const geometry = new THREE.CylinderGeometry( 8.5, 8.5, 4, 64 );
+    // const material = new THREE.MeshNormalMaterial( {color: 0x0095DD} );
+    // spinner = new THREE.Mesh( geometry, material );
+    // spinner.position.set(0, 24.8, 0);
+    // spinner.rotation.y += (Math.PI / 6); // set initial rotation so there is a flat side facing forward
+
+    // // add to spinner group
+    // spinnerGroup.add(spinner);
+
+    // //-----------------
+    // //--- Menu Text ---
+    // //-----------------
+
+    // // custom font loader
+    // const fontLoader = new FontLoader();
+    // const ttfLoader = new TTFLoader();
+    // ttfLoader.load('fonts3D/jet_brains_mono_regular.ttf', (json) => {
+    //   // parse the custom font
+    //   const jetBrainsFont = fontLoader.parse(json);
+
+    //   // Use parsed font as normal text geometry
+    //   const textGeometry = new TextGeometry('Pzzl   Gzzl', {
+    //     height: 150,
+    //     size: 10,
+    //     font: jetBrainsFont,
+    //   });
+    //   const textMaterial = new THREE.MeshNormalMaterial();
+    //   const textMesh = new THREE.Mesh(textGeometry, textMaterial);
+    //   textMesh.position.x = -46;
+    //   textMesh.position.y = 19;
+    //   textMesh.position.z = -150;
+    //   // add mesh to scene
+    //   scene.add( textMesh );
+    // });
+
+    // ttfLoader.load('fonts3D/jet_brains_mono_regular.ttf', (json) => {
+    //   // parse the custom font
+    //   const jetBrainsFont = fontLoader.parse(json);
+
+    //   // Use parsed font as normal text geometry
+    //   const textGeometry = new TextGeometry('sudoku', {
+    //     height: 1,
+    //     size: 2,
+    //     font: jetBrainsFont,
+    //   });
+    //   const textMaterial = new THREE.MeshLambertMaterial({color: 0xD9DCE2});
+    //   const textMesh = new THREE.Mesh(textGeometry, textMaterial);
+    //   textMesh.position.x = -5;
+    //   textMesh.position.y = 23.8;
+    //   textMesh.position.z = 8;
+
+    //   // add to spinner group
+    //   spinnerGroup.add(textMesh);
+    // });
+
+    // ttfLoader.load('fonts3D/jet_brains_mono_regular.ttf', (json) => {
+    //   // parse the custom font
+    //   const jetBrainsFont = fontLoader.parse(json);
+
+    //   // Use parsed font as normal text geometry
+    //   const textGeometry = new TextGeometry('word search', {
+    //     height: 1,
+    //     size: 1.3,
+    //     font: jetBrainsFont,
+    //   });
+    //   const textMaterial = new THREE.MeshLambertMaterial({color: 0xD9DCE2});
+    //   const textMesh = new THREE.Mesh(textGeometry, textMaterial);
+    //   textMesh.position.x = 5.8;
+    //   textMesh.position.y = 24.2;
+    //   textMesh.position.z = -8;
+    //   textMesh.rotation.y = Math.PI;
+
+    //   // add to spinner group
+    //   spinnerGroup.add(textMesh);
+    // });
+
+    // // add spinner group to scene
+    // scene.add(spinnerGroup);
+
+    // // load text for pzzl-bot
+    // ttfLoader.load('fonts3D/jet_brains_mono_regular.ttf', (json) => {
+    //   // parse the custom font
+    //   const jetBrainsFont = fontLoader.parse(json);
+
+    //   // Use parsed font as normal text geometry
+    //   const textGeometry = new TextGeometry('o  o', {
+    //     height: 0.2,
+    //     size: 2,
+    //     font: jetBrainsFont,
+    //   });
+    //   const textMaterial = new THREE.MeshLambertMaterial({color: 0x1E1C1B});
+    //   const textMesh = new THREE.Mesh(textGeometry, textMaterial);
+    //   textMesh.position.x = -2.9;
+    //   textMesh.position.y = 28;
+    //   textMesh.position.z = 10;
+
+    //   // add to spinner group
+    //   scene.add(textMesh);
+    // });
+
+    // // load text for pzzl-bot
+    // ttfLoader.load('fonts3D/jet_brains_mono_regular.ttf', (json) => {
+    //   // parse the custom font
+    //   const jetBrainsFont = fontLoader.parse(json);
+
+    //   // Use parsed font as normal text geometry
+    //   const textGeometry = new TextGeometry('-  -', {
+    //     height: 0.2,
+    //     size: 2,
+    //     font: jetBrainsFont,
+    //   });
+    //   const textMaterial = new THREE.MeshLambertMaterial({color: 0x1E1C1B});
+    //   const textMesh = new THREE.Mesh(textGeometry, textMaterial);
+    //   textMesh.position.x = -2.9;
+    //   textMesh.position.y = 28;
+    //   textMesh.position.z = 10;
+
+    //   // add to spinner group
+    //   scene.add(textMesh);
+    // });
+
+    // // load text for pzzl-bot
+    // ttfLoader.load('fonts3D/jet_brains_mono_regular.ttf', (json) => {
+    //   // parse the custom font
+    //   const jetBrainsFont = fontLoader.parse(json);
+
+    //   // Use parsed font as normal text geometry
+    //   const textGeometry = new TextGeometry('‿', {
+    //     height: 0.5,
+    //     size: 2,
+    //     font: jetBrainsFont,
+    //   });
+    //   const textMaterial = new THREE.MeshLambertMaterial({color: 0x1E1C1B});
+    //   const textMesh = new THREE.Mesh(textGeometry, textMaterial);
+    //   textMesh.position.x = -0.4;
+    //   textMesh.position.y = 28.8;
+    //   textMesh.position.z = 10;
+
+    //   // add to spinner group
+    //   scene.add(textMesh);
+    // });
+
+    // generate backgroud puzzle pieces
+
+    // for (let i = 0; i < 500; i++) {
+    //   // eslint-disable-next-line no-loop-func
+    //   loader.load('models/puzzle_piece.gltf', (gltf) => {
+    //       var object = gltf.scene;
+    //       object.position.set((1 * generateRandom(-120, 120)), (1 * generateRandom(-100, 100)), (1 * generateRandom(-10, -100)));
+    //       object.scale.set((1 * generateRandom(0.05, 0.5)), (1 * generateRandom(0.05, 0.5)), (1 * generateRandom(0.05, 0.5)));
+    //       object.rotation.set(generateRandom(0, (2 * Math.PI)), generateRandom(0, (2 * Math.PI)), generateRandom(0, (2 * Math.PI)));
+    //       scene.add(object);
+    //   });
+    // }
 
   };
 
@@ -325,6 +523,10 @@ export default function Menu(props) {
     renderer.render(scene, camera);
   }
 
+  function onTransitionEnd( event ) {
+    event.target.remove();
+  }
+
   const onWindowResize = () => {
     camera.aspect = window.innerWidth / window.innerHeight;
     camera.updateProjectionMatrix();
@@ -339,6 +541,9 @@ export default function Menu(props) {
   return (
     <>
       <div className='menu-3d'></div>
+      <section id="loading-screen">
+	      <div id="loader"></div>
+      </section>
       { showInstructions ? <Instructions message='Try again!' hideInstructions={() => setShowInstructions(false)} /> : <></>}
       <Help showHelp={showHelp} hideHelpPopup={() => setShowHelp(false)} />
       <Footer showHelpPopup={() => setShowHelp(true)} />
